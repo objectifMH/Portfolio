ed43bdf5e46b5bf7c516fe33ba76ce3483b35299
unknown <objectifmh@hotmail.com>
2020-10-26 22:15:34 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+  isShowMenu = false; 
+
+  faLinkedinIn = faLinkedinIn;
+  faGithub = faGithub;
+  
+  
+  getShowMenu() {
+    this.isShowMenu = !this.isShowMenu;
+    console.log(this.isShowMenu);
+  }
+
+
 }
